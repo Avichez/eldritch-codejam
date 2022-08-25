f0067ed2cd05802e53a5cba4c0b1647689d23135
Avichez <starkavichi@gmail.com>
2022-08-25 19:47:52 +0300

    feat: add choose animation for ancients card

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
 import "./index.html";
 import "./scss/style.scss";
+
+const ancients = document.querySelectorAll(".ancient-card");
+
+ancients.forEach((card) => {
+    card.addEventListener("click", activeAncient);
+});
+
+function activeAncient(event) {
+    removeSelect();
+    hideUnselected();
+    event.target.classList.remove("unselect");
+    event.target.classList.add("select");
+}
+
+function removeSelect() {
+    ancients.forEach((card) => {
+        card.classList.remove("select");
+    });
+}
+
+function hideUnselected() {
+    ancients.forEach((card) => {
+        if (!card.classList.contains("select")) {
+            card.classList.add("unselect");
+        }
+    });
+}
