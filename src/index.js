06a42d42fac25eed1b1646ebe9a8860440c81ff4
Avichez <starkavichi@gmail.com>
2022-08-28 20:47:56 +0300

    feat: add animation for open cards

--- src/index.js
+++ src/index.js
@@ -1,31 +1,3 @@
 import "./index.html";
 import "./scss/style.scss";
 import "./data/shuffleCards";
-
-// ancients choosing card animation
-// const ancients = document.querySelectorAll(".ancient-card");
-
-// ancients.forEach((card) => {
-//     card.addEventListener("click", selectAncient);
-// });
-
-// function selectAncient(event) {
-//     removeSelect();
-//     hideUnselected();
-//     event.target.classList.remove("unselect");
-//     event.target.classList.add("select");
-// }
-
-// function removeSelect() {
-//     ancients.forEach((card) => {
-//         card.classList.remove("select");
-//     });
-// }
-
-// function hideUnselected() {
-//     ancients.forEach((card) => {
-//         if (!card.classList.contains("select")) {
-//             card.classList.add("unselect");
-//         }
-//     });
-// }
